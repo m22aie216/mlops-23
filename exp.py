75c627cb2eb2b26313650398cb6a70a3085ea145
m22aie216 <142084927+m22aie216@users.noreply.github.com>
2023-09-16 16:35:09 +0530

    assignment3

--- exp.py
+++ exp.py
@@ -16,24 +16,38 @@ hand-written digits, from 0-9.
 
 # Import datasets, classifiers and performance metrics
 from sklearn import metrics, svm
-from utils import preprocess_data, split_data, train_model, read_digits, predict_and_eval, train_test_dev_split
+
+from utils import preprocess_data, split_data, train_model, read_digits, predict_and_eval, train_test_dev_split, get_hyperparameter_combinations, tune_hparams
 
 # 1. Get the dataset
 X, y = read_digits()
 
-# 3. Data splitting -- to create train and test sets
+# 2. Hyperparameter combinations
+# 2.1. SVM
+gamma_list = [0.001, 0.01, 0.1, 1]
+C_list = [1, 10, 100, 1000]
+h_params={}
+h_params['gamma'] = gamma_list
+h_params['C'] = C_list
+h_params_combinations = get_hyperparameter_combinations(h_params)
 
-# X_train, X_test, y_train, y_test = split_data(X,y, test_size=0.3)
-X_train, X_test, y_train, y_test, X_dev, y_dev = train_test_dev_split(X, y, test_size=0.3, dev_size=0.2)
-# 4. Data preprocessing
-X_train = preprocess_data(X_train)
-X_test = preprocess_data(X_test)
+test_sizes =  [0.1, 0.2, 0.3, 0.45]
+dev_sizes  =  [0.1, 0.2, 0.3, 0.45]
+for test_size in test_sizes:
+    for dev_size in dev_sizes:
+        train_size = 1- test_size - dev_size
+        # 3. Data splitting -- to create train and test sets                
+        X_train, X_test, X_dev, y_train, y_test, y_dev = train_test_dev_split(X, y, test_size=test_size, dev_size=dev_size)
+        # 4. Data preprocessing
+        X_train = preprocess_data(X_train)
+        X_test = preprocess_data(X_test)
+        X_dev = preprocess_data(X_dev)
+    
+        best_hparams, best_model, best_accuracy  = tune_hparams(X_train, y_train, X_dev, 
+        y_dev, h_params_combinations)
 
-# 5. Model training
-model = train_model(X_train, y_train, {'gamma': 0.001}, model_type="svm")
+        test_acc = predict_and_eval(best_model, X_test, y_test)
+        train_acc = predict_and_eval(best_model, X_train, y_train)
+        dev_acc = best_accuracy
 
-# 6. Getting model predictions on test set
-# Predict the value of the digit on the test subset
-# 7. Qualitative sanity check of the predictions
-# 8. Evaluation
-predict_and_eval(model, X_test, y_test)
+        print("test_size={:.2f} dev_size={:.2f} train_size={:.2f} train_acc={:.2f} dev_acc={:.2f} test_acc={:.2f}".format(test_size, dev_size, train_size, train_acc, dev_acc, test_acc))
