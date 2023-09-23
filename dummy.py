6ec32ccc8054811b6e6531ad7fb89fadd58c9e23
m22aie216 <142084927+m22aie216@users.noreply.github.com>
2023-09-23 13:49:20 +0530

    feature/dummy

---
+++ dummy.py
@@ -0,0 +1,22 @@
+"""
+================================
+Recognizing hand-written digits
+================================
+
+This example shows how scikit-learn can be used to recognize images of
+hand-written digits, from 0-9.
+
+"""
+
+# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
+# License: BSD 3 clause
+
+# Import datasets, classifiers and performance metrics
+from sklearn import metrics, svm
+
+from utils import preprocess_data, split_data, train_model, read_digits, predict_and_eval, train_test_dev_split, get_hyperparameter_combinations, tune_hparams
+
+# 1. Get the dataset
+X, y = read_digits()
+print('no of dataset,height and width')
+print(X.shape)
