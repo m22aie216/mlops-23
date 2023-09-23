d4481974a736b6ff90da1f0aa31a16607552bba6
m22aie216 <142084927+m22aie216@users.noreply.github.com>
2023-09-23 13:50:51 +0530

    dummy

--- exp.py
+++ exp.py
@@ -18,33 +18,5 @@ from utils import preprocess_data, split_data, train_model, read_digits, predict
 
 # 1. Get the dataset
 X, y = read_digits()
-
-# 2. Hyperparameter combinations
-# 2.1. SVM
-gamma_list = [0.001, 0.01, 0.1, 1]
-C_list = [1, 10, 100, 1000]
-h_params={}
-h_params['gamma'] = gamma_list
-h_params['C'] = C_list
-h_params_combinations = get_hyperparameter_combinations(h_params)
-
-test_sizes =  [0.1, 0.2, 0.3, 0.45]
-dev_sizes  =  [0.1, 0.2, 0.3, 0.45]
-for test_size in test_sizes:
-    for dev_size in dev_sizes:
-        train_size = 1- test_size - dev_size
-        # 3. Data splitting -- to create train and test sets                
-        X_train, X_test, X_dev, y_train, y_test, y_dev = train_test_dev_split(X, y, test_size=test_size, dev_size=dev_size)
-        # 4. Data preprocessing
-        X_train = preprocess_data(X_train)
-        X_test = preprocess_data(X_test)
-        X_dev = preprocess_data(X_dev)
-    
-        best_hparams, best_model, best_accuracy  = tune_hparams(X_train, y_train, X_dev, 
-        y_dev, h_params_combinations)
-
-        test_acc = predict_and_eval(best_model, X_test, y_test)
-        train_acc = predict_and_eval(best_model, X_train, y_train)
-        dev_acc = best_accuracy
-
-        print("test_size={:.2f} dev_size={:.2f} train_size={:.2f} train_acc={:.2f} dev_acc={:.2f} test_acc={:.2f}".format(test_size, dev_size, train_size, train_acc, dev_acc, test_acc))
+print('no of dataset,height and width')
+print(X.shape)
