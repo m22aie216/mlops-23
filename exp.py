0e7b89c474eb7c980b60af64c7c39bddf1b8adec
m22aie216 <142084927+m22aie216@users.noreply.github.com>
2023-09-23 17:43:47 +0530

    resize

--- exp.py
+++ exp.py
@@ -1,51 +1,49 @@
-"""
-================================
-Recognizing hand-written digits
-================================
-
-This example shows how scikit-learn can be used to recognize images of
-hand-written digits, from 0-9.
-
-"""
+# This example shows how scikit-learn can be used to recognize images of hand-written digits, from 0-9.
 
 # Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
 # License: BSD 3 clause
 
+
 # Import datasets, classifiers and performance metrics
-from sklearn import metrics, svm
+from utils import preprocess_data, tune_hparams, split_train_dev_test,read_digits,predict_and_eval,scale_image
 
-from utils import preprocess_data, split_data, train_model, read_digits, predict_and_eval, train_test_dev_split, get_hyperparameter_combinations, tune_hparams
 
-# 1. Get the dataset
-X, y = read_digits()
+# The digits dataset consists of 8x8 pixel images of digits. The images attribute of the dataset stores 8x8 arrays of grayscale values for each image. We will use these arrays to visualize the first 4 images. The target attribute of the dataset stores the digit each image represents and this is included in the title of the 4 plots below.
+# Note: if we were working from image files (e.g., ‘png’ files), we would load them using matplotlib.pyplot.imread.
 
-# 2. Hyperparameter combinations
-# 2.1. SVM
-gamma_list = [0.001, 0.01, 0.1, 1]
-C_list = [1, 10, 100, 1000]
-h_params={}
-h_params['gamma'] = gamma_list
-h_params['C'] = C_list
-h_params_combinations = get_hyperparameter_combinations(h_params)
+# 1. Data Loading
 
-test_sizes =  [0.1, 0.2, 0.3, 0.45]
-dev_sizes  =  [0.1, 0.2, 0.3, 0.45]
-image_size="4x4"
-for test_size in test_sizes:
-    for dev_size in dev_sizes:
-        train_size = 1- test_size - dev_size
-        # 3. Data splitting -- to create train and test sets                
-        X_train, X_test, X_dev, y_train, y_test, y_dev = train_test_dev_split(X, y, test_size=test_size, dev_size=dev_size)
-        # 4. Data preprocessing
-        X_train = preprocess_data(X_train)
-        X_test = preprocess_data(X_test)
-        X_dev = preprocess_data(X_dev)
-   	#image_size = X_test.shape 
-        best_hparams, best_model, best_accuracy  = tune_hparams(X_train, y_train, X_dev, 
-        y_dev, h_params_combinations)
+x,y = read_digits()
 
-        test_acc = predict_and_eval(best_model, X_test, y_test)
-        train_acc = predict_and_eval(best_model, X_train, y_train)
-        dev_acc = best_accuracy
-	
-        print(" image_size={:} test_size={:.2f} dev_size={:.2f} train_size={:.2f} train_acc={:.2f} dev_acc={:.2f} test_acc={:.2f}".format(image_size, test_size, dev_size, train_size, train_acc, dev_acc, test_acc))
+
+print("Total number of samples : ", len(x))
+
+print("(number of samples,length of image,height of image) is:",x.shape)
+
+test_sizes = [0.2]
+dev_sizes = [0.1]
+image_dimension = [8,6,4]
+
+for dimension in image_dimension:
+    x_resize = scale_image(x,dimension)
+    for test_size in test_sizes:
+        for dev_size in dev_sizes:
+            # 3. Data splitting
+            X_train, X_test,X_dev, y_train, y_test,y_dev = split_train_dev_test(x_resize, y, test_size=test_size, dev_size=dev_size);
+
+            # 4. Data Preprocessing
+            X_train = preprocess_data(X_train)
+            X_test = preprocess_data(X_test)
+            X_dev = preprocess_data(X_dev)
+
+            gama_ranges = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
+            C_ranges = [0.1,1,2,5,10]
+            list_of_all_param_combination = [{'gamma': gamma, 'C': C} for gamma in gama_ranges for C in C_ranges]
+
+            # Predict the value of the digit on the test subset
+            # 6.Predict and Evaluate 
+            best_hparams, best_model, best_accuracy = tune_hparams(X_train, y_train, X_dev, y_dev, list_of_all_param_combination)
+            accuracy_test = predict_and_eval(best_model, X_test, y_test)
+            accuracy_dev = predict_and_eval(best_model, X_dev, y_dev)
+            accuracy_train = predict_and_eval(best_model, X_train, y_train)
+            print(f"image_size={dimension}x{dimension},test_size={test_size} dev_size={dev_size} train_size={1- (dev_size+test_size)} train_acc={accuracy_train} dev_acc={accuracy_dev} test_acc={accuracy_test}")
