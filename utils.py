50289fcf6a71817bd1f3c31bf2e7e2cbdedec3c9
m22aie216 <142084927+m22aie216@users.noreply.github.com>
2023-09-30 21:10:29 +0530

    added best_model_path

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from sklearn.model_selection import train_test_split
 from sklearn import svm, datasets, metrics
+from joblib import dump, load
 # we will put all utils here
 
 def get_combinations(param_name, param_values, base_combinations):    
@@ -18,6 +19,7 @@ def get_hyperparameter_combinations(dict_of_param_lists):
 
 def tune_hparams(X_train, y_train, X_dev, y_dev, h_params_combinations):
     best_accuracy = -1
+    best_model_path = ""
     for h_params in h_params_combinations:
         # 5. Model training
         model = train_model(X_train, y_train, h_params, model_type="svm")
@@ -26,9 +28,15 @@ def tune_hparams(X_train, y_train, X_dev, y_dev, h_params_combinations):
         if cur_accuracy > best_accuracy:
             best_accuracy = cur_accuracy
             best_hparams = h_params
+            best_model_path = "./models/best_model" +"_".join(["{}:{}".format(k,v) for k,v in h_params.items()]) + ".joblib"
             best_model = model
 
-    return best_hparams, best_model, best_accuracy 
+    # save the best_model    
+    dump(best_model, best_model_path) 
+
+    print("Model save at {}".format(best_model_path))
+
+    return best_hparams, best_model_path, best_accuracy 
 
 
 
@@ -47,7 +55,7 @@ def preprocess_data(data):
 # Split data into 50% train and 50% test subsets
 def split_data(x, y, test_size, random_state=1):
     X_train, X_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.5,random_state=random_state
+    x, y, test_size=test_size,random_state=random_state
     )
     return X_train, X_test, y_train, y_test
 
@@ -63,8 +71,11 @@ def train_model(x, y, model_params, model_type="svm"):
 
 
 def train_test_dev_split(X, y, test_size, dev_size):
-    X_train_dev, X_test, Y_train_Dev, y_test =  train_test_split(X, y, test_size=test_size, random_state=1)
+    X_train_dev, X_test, Y_train_Dev, y_test =  split_data(X, y, test_size=test_size, random_state=1)
+    print("train+dev = {} test = {}".format(len(Y_train_Dev),len(y_test)))
+    
     X_train, X_dev, y_train, y_dev = split_data(X_train_dev, Y_train_Dev, dev_size/(1-test_size), random_state=1)
+        
     return X_train, X_test, X_dev, y_train, y_test, y_dev
 
 # Question 2:
