5a02200a09e737192fd45e740b7564ba845a8a29
m22aie216 <142084927+m22aie216@users.noreply.github.com>
2023-11-02 23:03:29 +0530

    modified

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from sklearn.model_selection import train_test_split
-from sklearn import svm, datasets, metrics
+from sklearn import svm, tree, datasets, metrics
 from joblib import dump, load
 # we will put all utils here
 
@@ -17,24 +17,23 @@ def get_hyperparameter_combinations(dict_of_param_lists):
         base_combinations = get_combinations(param_name, param_values, base_combinations)
     return base_combinations
 
-def tune_hparams(X_train, y_train, X_dev, y_dev, h_params_combinations):
+def tune_hparams(X_train, y_train, X_dev, y_dev, h_params_combinations, model_type="svm"):
     best_accuracy = -1
     best_model_path = ""
     for h_params in h_params_combinations:
         # 5. Model training
-        model = train_model(X_train, y_train, h_params, model_type="svm")
+        model = train_model(X_train, y_train, h_params, model_type=model_type)
         # Predict the value of the digit on the test subset        
-        cur_accuracy = predict_and_eval(model, X_dev, y_dev)
+        cur_accuracy, _, _ = predict_and_eval(model, X_dev, y_dev)
         if cur_accuracy > best_accuracy:
             best_accuracy = cur_accuracy
             best_hparams = h_params
-            best_model_path = "./models/best_model" +"_".join(["{}:{}".format(k,v) for k,v in h_params.items()]) + ".joblib"
+            best_model_path = "./models/{}_".format(model_type) +"_".join(["{}:{}".format(k,v) for k,v in h_params.items()]) + ".joblib"
             best_model = model
 
     # save the best_model    
     dump(best_model, best_model_path) 
 
-    print("Model save at {}".format(best_model_path))
 
     return best_hparams, best_model_path, best_accuracy 
 
@@ -55,7 +54,7 @@ def preprocess_data(data):
 # Split data into 50% train and 50% test subsets
 def split_data(x, y, test_size, random_state=1):
     X_train, X_test, y_train, y_test = train_test_split(
-    x, y, test_size=test_size,random_state=random_state
+    x, y, test_size=test_size, shuffle = True
     )
     return X_train, X_test, y_train, y_test
 
@@ -64,6 +63,9 @@ def train_model(x, y, model_params, model_type="svm"):
     if model_type == "svm":
         # Create a classifier: a support vector classifier
         clf = svm.SVC
+    if model_type == "tree":
+        # Create a classifier: a decision tree classifier
+        clf = tree.DecisionTreeClassifier
     model = clf(**model_params)
     # train the model
     model.fit(x, y)
@@ -81,4 +83,4 @@ def train_test_dev_split(X, y, test_size, dev_size):
 # Question 2:
 def predict_and_eval(model, X_test, y_test):
     predicted = model.predict(X_test)
-    return metrics.accuracy_score(y_test, predicted)
+    return metrics.accuracy_score(y_test, predicted), metrics.f1_score(y_test, predicted, average="macro"), predicted
