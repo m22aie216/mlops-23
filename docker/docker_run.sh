07d15913dc882c128dce636b94157800b0be2976
m22aie216 <142084927+m22aie216@users.noreply.github.com>
2023-11-16 21:14:35 +0530

    updated

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-docker build -t exp:v1 -f docker/Dockerfile .
-docker run -it exp:v1
+docker build -t exp:v1 -f ./docker/Dockerfile .
+docker volume create train_mod
+docker run -v train_mod:/exp/models exp:v1
