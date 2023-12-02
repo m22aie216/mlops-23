36455fb7888c168e1a5d3d416f1e1923bc36ef84
m22aie216 <142084927+m22aie216@users.noreply.github.com>
2023-12-02 19:20:24 +0530

    Q5. docker files

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker build -t exp:v1 -f ./docker/Dockerfile .
-docker volume create train_mod
-docker run -d -p 80:5000 -v train_mod:/exp/models exp:v1
+docker build -t base:latest -f ./docker/DependencyDockerfile .
+docker build -t exp:latest -f ./docker/FinalDockerfile .
+docker run exp:latest
